f6bb801b9819a941bfb73a502c40ed5c429c12ee
Abi Patchaiyappan <abi2rami@gmail.com>
2020-06-04 20:13:04 -0700

    deploy to appengine and add facts about me

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['My name is Abi!', 'I love Stranger Things', 'I have pet goldfish', 'I am a Scorpio'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
